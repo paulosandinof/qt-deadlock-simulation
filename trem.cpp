21bfadaa539f78af038c894dd2cf5fd676aa7ea5
Sandino <sandino@pop-os.localdomain>
2021-03-19 17:40:43 -0300

    Initial version

---
+++ trem.cpp
@@ -0,0 +1,81 @@
+#include "trem.h"
+#include <QtCore>
+#include <iostream>
+
+//Construtor
+Trem::Trem(int ID, int x, int y){
+    this->ID = ID;
+    this->x = x;
+    this->y = y;
+    velocidade = 100;
+}
+
+//Função a ser executada após executar trem->START
+void Trem::run(){
+    while(true){
+        switch(ID){
+        case 1:     //Trem 1
+            if (y == 30 && x < 330)
+                x+=10;
+            else if (x == 330 && y < 150)
+                y+=10;
+            else if (x > 60 && y == 150)
+                x-=10;
+            else
+                y-=10;
+            emit updateGUI(ID, x,y);    //Emite um sinal
+            break;
+        case 2: //Trem 2
+            if (y == 30 && x <600)
+                x+=10;
+            else if (x == 600 && y < 150)
+                y+=10;
+            else if (x > 330 && y == 150)
+                x-=10;
+            else
+                y-=10;
+            emit updateGUI(ID, x,y);    //Emite um sinal
+            break;
+        case 3:
+            if (y == 30 && x < 870)
+                x+=10;
+            else if (x == 870 && y < 150)
+                y+=10;
+            else if (x > 600 && y == 150)
+                x-=10;
+            else
+                y-=10;
+            emit updateGUI(ID, x,y);
+            break;
+        case 4:
+            if (y == 280 && x > 470)
+                x-=10;
+            else if (x == 470 && y > 150)
+                y-=10;
+            else if (y == 150 && x < 740)
+                x+=10;
+            else
+                y+=10;
+            emit updateGUI(ID, x,y);
+            break;
+        case 5:
+            if (y == 280 && x > 200)
+                x-=10;
+            else if (x == 200 && y > 150)
+                y-=10;
+            else if (y == 150 && x < 470)
+                x+=10;
+            else
+                y+=10;
+            emit updateGUI(ID, x,y);
+            break;
+        default:
+            break;
+        }
+        msleep(velocidade);
+    }
+}
+
+
+
+
